f81f16908eb4b143dc279b02423bc1ba9ae68946
ismat461 <62961264+ismat461@users.noreply.github.com>
2020-05-07 18:18:19 -0400

    Update timerscreen.js

--- src/login/timerscreen.js
+++ src/login/timerscreen.js
@@ -1,73 +1,79 @@
-import React from 'react';
-import {StyleSheet,Text,Keyboard,View,TextInput,TouchableWithoutFeedback,Alert,KeyboardAvoidingView,Button,Image, TouchableHighlight,ImageBackground,Platform,Dimensions
-} from 'react-native';
+import React, { useState, useEffect, Component } from 'react';
+import { StyleSheet, Text, View, StatusBar, TouchableOpacity, Dimensions, TouchableHighlight } from 'react-native';
 
 let deviceHeight = Dimensions.get('window').height;
 let deviceWidth = Dimensions.get('window').width;
 
-export class Timer extends React.Component {
+const formatNumber = number => `0${number}`.slice(-2);
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.textContainer}>
-          <Text style={styles.titleText}>
-            Timer
-          </Text>
-        </View>
+const getRemaining = (time) => {
+    const mins = Math.floor(time / 60);
+    const secs = time - mins * 60;
+    return { mins: formatNumber(mins), secs: formatNumber(secs) };
+}
 
+export default function Timer() {
 
-        <View style={styles.textContainer}>
-          <Text style={styles.timerText}>
-            0:00:00
-          </Text>    
-        </View>
+  const [remainingSecs, setRemainingSecs] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const { mins, secs } = getRemaining(remainingSecs);
 
-         <View style={styles.buttonContainer}>
-            <TouchableHighlight
-              onPress={() => this.props.navigation.navigate('Home')}
-            >
-                <View style={styles.touchableButton1}>
-                  <Text style={styles.buttonText}>
-                    Start timer
-                  </Text>
-                </View>
-            </TouchableHighlight>
-            <TouchableHighlight
-              onPress={() => this.props.navigation.navigate('Home')}
-            >
-                <View style={styles.touchableButton1}>
-                  <Text style={styles.buttonText}>
-                   Homescreen
-                  </Text>
-                </View>
-            </TouchableHighlight>
-            <TouchableHighlight
-              onPress={() => this.props.navigation.navigate('Genre')}
-            >
-                <View style={styles.touchableButton1}>
-                  <Text style={styles.buttonText}>
-                   Genre Selection
-                  </Text>
-                </View>
-            </TouchableHighlight>
-        </View>
-
- 
-
-      </View>
-    );
+  toggle = () => {
+    setIsActive(!isActive);
   }
+
+  reset = () => {
+    setRemainingSecs(0);
+    setIsActive(false);
+  }
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setRemainingSecs(remainingSecs => remainingSecs + 1);
+      }, 1000);
+    } else if (!isActive && remainingSecs !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, remainingSecs]);
+
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.center}>
+          <View style={styles.textContainer}>
+            <Text style={styles.titleText}>
+              Timer
+            </Text>
+          </View>
+          <View style={styles.buttonContainer}>
+            <StatusBar barStyle="light-content" />
+            <Text style={styles.timerText}>{`${mins}:${secs}`}</Text>
+            <TouchableOpacity onPress={this.toggle} style={styles.touchableButton1}>
+                <Text style={styles.buttonText}>{isActive ? 'Pause' : 'Start'}</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={this.reset} style={[styles.touchableButton1, styles.buttonReset]}>
+                <Text style={[styles.buttonText, styles.buttonTextReset]}>Reset</Text>
+            </TouchableOpacity>
+
+            
+          </View>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
-    justifyContent:'space-around',
+    alignItems: 'center',
+    justifyContent:'center',
+    
+    
   },
-
-
   textContainer: {
     justifyContent: 'center',
     alignItems: 'center',
@@ -77,35 +83,49 @@ const styles = StyleSheet.create({
     fontWeight:'bold',
     justifyContent: 'center',
     alignItems: 'center',  
-    fontSize:20,
+    fontSize:40,
+    textAlign:'center',
   },
 
    timerText:{
     justifyContent: 'center',
     alignItems: 'center',  
-    fontSize:20,
+    fontSize:30,
 
   },
-
-  text: {
-    fontSize: 8,
-    fontWeight:'bold',
-    justifyContent: 'center',
+  touchableButton1: {
+    height: deviceHeight/15,
+    width: deviceWidth/1.6,
     alignItems: 'center',
-  },
-
-   touchableButton1: {
-        height: 40,
-        width: 200,
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 10,
-        backgroundColor: '#42AFC2',
-        margin:20,
+    justifyContent: 'center',
+    borderRadius: 10,
+    backgroundColor: '#42AFC2',
+    marginTop:10
     },
+
+  touchableButton2: {
+  height: deviceHeight/15,
+    width: deviceWidth/1.6,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    backgroundColor: '#42AFC2',
+    marginTop:10
+   },
+
+   center:{
+     alignItems:'center',
+     justifyContent:'center',
+   },
 
     buttonContainer:{
       height:deviceHeight/3,
+      justifyContent:'center',
+      alignItems:'center',
+    },
+
+    buttonContainer2:{
+      height:deviceHeight/5,
       justifyContent:'center',
       alignItems:'center',
     },
@@ -115,6 +135,3 @@ const styles = StyleSheet.create({
         fontWeight:'bold',
     }
 });
-
-
-export default Timer;
