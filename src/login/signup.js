d55e19e5b835a66ee19e51624728c96a2bd1fc9f
ismat461 <62961264+ismat461@users.noreply.github.com>
2020-05-08 05:59:47 -0400

    Update signup.js

--- src/login/signup.js
+++ src/login/signup.js
@@ -1,128 +1,137 @@
-     
-import React, { Component } from "react";
+import React, { useState, useEffect, Component } from 'react';
+import { StyleSheet, Text, View, StatusBar, TouchableOpacity, Dimensions, TouchableHighlight } from 'react-native';
 
+let deviceHeight = Dimensions.get('window').height;
+let deviceWidth = Dimensions.get('window').width;
 
-import {Keyboard, Text, View, TextInput, TouchableWithoutFeedback, Alert, KeyboardAvoidingView, Expo,Platform, StyleSheet, Image, Button, TouchableHighlight, Dimensions,TouchableOpacity} from 'react-native';
+const formatNumber = number => `0${number}`.slice(-2);
 
-
-class Signup extends React.Component {
-
-
-  render() {
-     let deviceHeight = Dimensions.get('window').height;
-     let deviceWidth = Dimensions.get('window').width;
-
-    return (
-      <KeyboardAvoidingView style={styles.containerView} behavior="padding">
-
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View style={styles.loginScreenContainer}>
-          <View style={styles.loginFormView}>
-
- <View style={styles.textContainer}>
-
- <Image source ={{uri: 'https://codehs.com/uploads/1a3f40510a74f221c76bdcbd442e9762'}}
-          style={{ height: deviceHeight/8, width:deviceWidth/2, justifyContent: 'center',  }}
-          />
-
-</View>
-
-             <TextInput placeholder="Email" placeholderColor="#c4c3cb" style={styles.loginFormTextInput} />
-            
-            <TextInput placeholder="Password" placeholderColor="#c4c3cb" style={styles.loginFormTextInput} secureTextEntry={true}/>
-
-             <View style={styles.textContainer}>
-            <TouchableHighlight
-              onPress={() => this.props.navigation.navigate('Timer')}
-            >
-           <View style={styles.touchableButton1}>
-                        <Text style={styles.buttonText}>
-                            CONTINUE
-                        </Text>
-                    </View>
-                </TouchableHighlight>
-            </View>
-
-
-
-
-
- <View style={styles.textContainer}>
-
-
- <Button
-          title="DONT HAVE AN ACCOUNT ? SIGN UP "
-          onPress={() => this.props.navigation.navigate('Email')}
-        />
-          
-          </View>
-
-
-          </View>
-        </View>
-      </TouchableWithoutFeedback>
-      </KeyboardAvoidingView>
-    );
-  }
-
+const getRemaining = (time) => {
+    const mins = Math.floor(time / 60);
+    const secs = time - mins * 60;
+    return { mins: formatNumber(mins), secs: formatNumber(secs) };
 }
 
+export default function Timer() {
 
+  const [remainingSecs, setRemainingSecs] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const { mins, secs } = getRemaining(remainingSecs);
+
+  toggle = () => {
+    setIsActive(!isActive);
+  }
+
+  reset = () => {
+    setRemainingSecs(0);
+    setIsActive(false);
+  }
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setRemainingSecs(remainingSecs => remainingSecs + 1);
+      }, 1000);
+    } else if (!isActive && remainingSecs !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, remainingSecs]);
+
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.center}>
+          <View style={styles.textContainer}>
+            <Text style={styles.titleText}>
+              Timer
+            </Text>
+          </View>
+          <View style={styles.buttonContainer}>
+            <StatusBar barStyle="light-content" />
+            <Text style={styles.timerText}>{`${mins}:${secs}`}</Text>
+            <TouchableOpacity onPress={this.toggle} style={styles.touchableButton1}>
+                <Text style={styles.buttonText}>{isActive ? 'Pause' : 'Start'}</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={this.reset} style={[styles.touchableButton1, styles.buttonReset]}>
+                <Text style={[styles.buttonText, styles.buttonTextReset]}>Reset</Text>
+            </TouchableOpacity>
+
+            
+          </View>
+      </View>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-
-containerView: {
-  flex: 1,
-  backgroundColor:'white',
-
-
-},
-loginScreenContainer: {
-  flex: 1,
-},
-textContainer: {
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent:'center',
+    
+    
+  },
+  textContainer: {
     justifyContent: 'center',
     alignItems: 'center',
+    height:deviceHeight/8,
+  },
+  titleText:{
+    fontWeight:'bold',
+    justifyContent: 'center',
+    alignItems: 'center',  
+    fontSize:40,
+    textAlign:'center',
   },
 
-loginFormView: {
-  flex: 1
-},
-loginFormTextInput: {
-  height: 43,
-  fontSize: 14,
-  borderRadius: 5,
-  borderWidth: 1,
-  borderColor: '#eaeaea',
-  backgroundColor: '#fafafa',
-  paddingLeft: 10,
-  marginLeft: 15,
-  marginRight: 15,
-  marginTop: 5,
-  marginBottom: 5,
+   timerText:{
+    justifyContent: 'center',
+    alignItems: 'center',  
+    fontSize:30,
 
-},
-
-touchableButton1: {
-        backgroundColor: '#42AFC2',
-        height: 40,
-        width: 200,
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: 100,
-        marginBottom: 50,
-         borderRadius: 10,
-    borderWidth: 1,
-    
-   
+  },
+  touchableButton1: {
+    height: deviceHeight/15,
+    width: deviceWidth/1.6,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    backgroundColor: '#42AFC2',
+    marginTop:10
     },
 
- buttonText: {
-     
-        color: 'white',
+  touchableButton2: {
+  height: deviceHeight/15,
+    width: deviceWidth/1.6,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    backgroundColor: '#42AFC2',
+    marginTop:10
+   },
+
+   center:{
+     alignItems:'center',
+     justifyContent:'center',
+   },
+
+    buttonContainer:{
+      height:deviceHeight/3,
+      justifyContent:'center',
+      alignItems:'center',
+    },
+
+    buttonContainer2:{
+      height:deviceHeight/5,
+      justifyContent:'center',
+      alignItems:'center',
+    },
+   
+    buttonText: {
         fontSize: 14,
         fontWeight:'bold',
- }
+    }
 });
-
-export default Signup;
